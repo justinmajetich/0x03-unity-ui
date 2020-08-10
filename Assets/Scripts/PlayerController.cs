40806d5757ab0eaea0e298bb61644cd8188532f2
justinmajetich <justinmajetich@gmail.com>
2020-08-10 16:07:37 -0400

    Create player controller for WASD movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+    private Rigidbody rb;
+    private float xForce = 0;
+    private float zForce = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Fixed-update is called once physics refresh
+    void FixedUpdate()
+    {
+        // Take force applied against horizontal axis (i.e left/right, a/d)
+        xForce = Input.GetAxis("Horizontal") * speed;
+        // Take force applied against vertical axis (i.e forward/backward, w/s)
+        zForce = Input.GetAxis("Vertical") * speed;
+
+        // Apply force to player rigidbody
+        rb.AddForce(xForce, 0, zForce); 
+    }
+}
